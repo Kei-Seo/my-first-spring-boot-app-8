bdc5a1f9d5fd9256bbc676ca6ccad416f68e9306
Kei-Seo <ghdtjr3824@naver.com>
2021-04-06 17:22:57 +0900

    Initial commit

---
+++ src/main/java/org/cnu/realcoding/homework/myfirstspringbootapp8/MyFirstSpringBootApp8Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.homework.myfirstspringbootapp8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootApp8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootApp8Application.class, args);
+	}
+
+}
